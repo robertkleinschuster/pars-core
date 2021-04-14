9af3e3d41a39dcf3f4b986e9800c6f5db5d7994f
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-04-14 12:34:25 +0200

    task log

---
+++ src/Database/DefaultBeanFieldTrait.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Pars\Core\Database;
+
+
+trait DefaultBeanFieldTrait
+{
+    public ?\DateTime $Timestamp_Create = null;
+    public ?\DateTime $Timestamp_Edit = null;
+    public ?int $Person_ID_Create = null;
+    public ?int $Person_ID_Edit = null;
+}
