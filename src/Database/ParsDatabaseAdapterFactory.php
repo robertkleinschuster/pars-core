c1117f4ef5c703c7c00097d5746107cdf529313b
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-05-16 17:53:26 +0200

    fix(Setup): Setup exception handling improvements

--- src/Database/ParsDatabaseAdapterFactory.php
+++ src/Database/ParsDatabaseAdapterFactory.php
@@ -6,12 +6,13 @@ namespace Pars\Core\Database;
 
 use Laminas\Db\Adapter\AdapterInterface;
 use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
 
 class ParsDatabaseAdapterFactory
 {
     public function __invoke(ContainerInterface $container)
     {
-        return new ParsDatabaseAdapter($container->get(AdapterInterface::class));
+        return new ParsDatabaseAdapter($container->get(AdapterInterface::class), $container->get(LoggerInterface::class));
     }
 
 }
