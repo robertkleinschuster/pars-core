a9dcd775f9fa01f21afb397047377159d2e98490
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-05-18 19:58:08 +0200

    fix(Updates): Updates

--- src/Translation/ParsTranslatorFactory.php
+++ src/Translation/ParsTranslatorFactory.php
@@ -2,11 +2,14 @@
 
 namespace Pars\Core\Translation;
 
+use Laminas\I18n\Translator\Loader\RemoteLoaderInterface;
+use Laminas\I18n\Translator\Translator;
 use Laminas\I18n\Translator\TranslatorInterface;
 use Pars\Core\Config\ParsConfig;
 use Pars\Core\Localization\LocaleFinderInterface;
 use Pars\Core\Localization\LocaleInterface;
 use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
 
 /**
  * Class ParsTranslatorFactory
@@ -16,19 +19,13 @@ class ParsTranslatorFactory
 {
     public function __invoke(ContainerInterface $container)
     {
-        $config = $container->get('config');
-        $saver = $container->get(MissingTranslationSaverInterface::class);
-        $namespace = ParsTranslator::NAMESPACE_DEFAULT;
-        if (isset($config['translator']['namespace'])) {
-            $namespace = $config['translator']['namespace'];
-        }
         return new ParsTranslator(
             $container->get(TranslatorInterface::class),
             $container->get(LocaleInterface::class),
-            $namespace,
-            $saver,
+            $container->get(MissingTranslationSaverInterface::class),
             $container->get(ParsConfig::class),
-            $container->get(LocaleFinderInterface::class)
+            $container->get(LocaleFinderInterface::class),
+            $container->get(LoggerInterface::class)
         );
     }
 
--- src/Translation/TranslatorMiddleware.php
+++ src/Translation/TranslatorMiddleware.php
@@ -2,17 +2,14 @@
 
 namespace Pars\Core\Translation;
 
-use Laminas\I18n\Translator\Loader\RemoteLoaderInterface;
-use Pars\Core\Config\ParsConfig;
 use Pars\Core\Container\ParsContainer;
 use Pars\Core\Container\ParsContainerAwareTrait;
 use Pars\Core\Localization\LocaleInterface;
-use Pars\Core\Logging\LoggingMiddleware;
+use Pars\Pattern\Exception\CoreException;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Psr\Log\LoggerInterface;
 
 /**
  * Class TranslatorMiddleware
@@ -20,24 +17,21 @@ use Psr\Log\LoggerInterface;
  */
 class TranslatorMiddleware implements MiddlewareInterface
 {
-
     use ParsContainerAwareTrait;
+
     /**
      * @var ParsTranslator
      */
     private ParsTranslator $translator;
 
-    private ParsConfig $config;
-
     /**
      * TranslatorMiddleware constructor.
      * @param ParsContainer $parsContainer
-     * @throws \Pars\Pattern\Exception\CoreException
+     * @throws CoreException
      */
     public function __construct(ParsContainer $parsContainer)
     {
         $this->translator = $parsContainer->getTranslator();
-        $this->config = $parsContainer->getConfig();
         $this->setParsContainer($parsContainer);
     }
 
@@ -50,37 +44,9 @@ class TranslatorMiddleware implements MiddlewareInterface
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $locale = $request->getAttribute(LocaleInterface::class);
-        $logger = $this->getParsContainer()->getLogger();
         if ($locale instanceof LocaleInterface) {
             $this->translator->setLocale($locale);
         }
-        $this->translator->getTranslator()->setFallbackLocale($this->config->get('locale.default'));
-        $this->translator->getTranslator()->enableEventManager();
-        if ($this->translator->getTranslator()->isEventManagerEnabled()) {
-            $this->translator->getTranslator()->getEventManager()->attach(
-                \Laminas\I18n\Translator\Translator::EVENT_MISSING_TRANSLATION,
-                function (\Laminas\EventManager\EventInterface $event) use ($logger) {
-                    $data = $event->getParams();
-                    if ($logger instanceof LoggerInterface) {
-                        $logger->warning('Missing translation', $data);
-                    }
-                    $this->translator->saveMissingTranslation($data['locale'], $data['message'], $data['text_domain']);
-                }
-            );
-            $this->translator->getTranslator()->getEventManager()->attach(
-                \Laminas\I18n\Translator\Translator::EVENT_NO_MESSAGES_LOADED,
-                function (\Laminas\EventManager\EventInterface $event) use ($logger) {
-                    if ($logger instanceof LoggerInterface) {
-                        $logger->error('No messages loaded', $event->getParams());
-                    }
-                }
-            );
-        }
-        $this->translator->getTranslator()
-            ->getPluginManager()
-            ->setFactory(RemoteLoaderInterface::class, function ($container) {
-                return $container->get(RemoteLoaderInterface::class);
-            });
         return $handler->handle($request);
     }
 }
