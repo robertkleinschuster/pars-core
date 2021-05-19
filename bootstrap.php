b733fc68fd6994d8737702150789802fc5825daf
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-05-19 18:40:09 +0200

    feat(Frontend): Cleanup

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,15 @@
 <?php
-define('PARS_DIR', getcwd());
+@define('PARS_DIR', getcwd());
 if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
     $decodedUri = urldecode($_SERVER['REQUEST_URI']);
     $uri = $_SERVER['REQUEST_URI'];
     if (count($_GET) == 0) {
-        if (file_exists(PARS_DIR . "/public$decodedUri") || file_exists(PARS_DIR . "/public$uri")) {
+        $file = PARS_DIR . "/public$decodedUri";
+        if (file_exists($file) && is_file($file)) {
+            return false;
+        }
+        $file = PARS_DIR . "/public$uri";
+        if (file_exists($file) && is_file($file)) {
             return false;
         }
     }
